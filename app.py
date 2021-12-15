366ea2842e8a6a1cdf5174da3a5ebef3a928426c
Yanhao Jin <yanhao.jin@outlook.com>
2021-12-15 04:41:09 +0000

    server - return recommendations

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import request
 import requests
 import wordcount
 import json
+import heapq
+from flask.json import jsonify
 
 app = Flask(__name__)
 url = "https://findwork.dev/api/jobs/"
@@ -13,37 +15,45 @@ CORS(app)
 def hello_world():
     return "hello world"
 
-@app.route("/get_jobs/<string:type>")
+@app.route("/get_jobs/<string:type>", methods=['POST'])
 def get_job(type):
-    payload={}
+    postJson = request.get_json()
+    if "skills" in postJson:
+        given_scores = postJson["skills"]
+    else :
+        return jsonify({
+            "error" : "please provide skills in the HTTP request body"
+        })
+    
+    # request target API and get responses
     get_job_url = url + "?search=" + type
-    headers = {'Authorization': 'Token 9fe56153c850546257b47713c1cb9810ce7d441b'}
-    response = requests.request("GET", get_job_url, headers=headers, data=payload)
-    results = []
-    responseDict = response.json()
+    results = get_page(get_job_url)
 
-    if "results" in responseDict:
-        results = responseDict["results"]
-    if "next" in responseDict:
-        next_url = responseDict["next"]
-        results = results + get_next_page(next_url)
-    returnResults = {}
-    returnResults["results"] = results
-    print(len(results))
+    # send each job information to the spark util 
     wc = wordcount.Wordcount()
     scores = []
-    given_scores = {
-        "swift" : 4,
-        "android" : 2,
-        "objectivec" : 3
-    }
-    for result in results:
-        score = wc.extract_keywords(json.dumps(result), given_scores)
-        scores.append(score)
+    for i in range(len(results)):
+        score = wc.extract_keywords(json.dumps(results[i]), given_scores)
+        heapq.heappush(scores, (score,i))
+    
+    #return results with top 20% score
+    recommend_num = int(len(results) * 0.2)
+    while len(scores) > recommend_num:
+        heapq.heappop(scores)
+    
+    recommend_results = []
+    for score, index in scores:
+        results[index]["score"] = score
+        recommend_results.append(results[index])
+    if len(recommend_results) == 0 :
+        return jsonify({
+            "error" : "cannot find matching jobs"
+        })
+    return jsonify(recommend_results)
 
-    return returnResults
 
-def get_next_page(get_job_url):
+# util method for requesting url and parsing json
+def get_page(get_job_url):
     payload={}
     headers = {'Authorization': 'Token 9fe56153c850546257b47713c1cb9810ce7d441b'}
     response = requests.request("GET", get_job_url, headers=headers, data=payload)
@@ -55,7 +65,7 @@ def get_next_page(get_job_url):
     if "next" in responseDict:
         next_url = responseDict["next"]
         if next_url is not None:
-            results = results + get_next_page(next_url)
+            results = results + get_page(next_url)
     return results
     
 if __name__ == '__main__':
--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,4 @@
-import sys
 from operator import add
-import pyspark
 from pyspark.sql import SparkSession
 from pyspark.mllib.feature import HashingTF, IDF
 from pyspark.context import SparkContext
@@ -11,20 +9,19 @@ import re
 class Wordcount:
     def __init__(self) -> None:
         self.sc = SparkContext('local')
-        #self.spark = SparkSession.builder.master("Cluster").appName(name).getOrCreate()
-        pass
+
+    # precess given text: remove html tags and special characters
     def pre_precess(self, text):
         text = text.lower()
         tagfree = re.compile('<.*?>')
         text = re.sub(tagfree, '', text)
-        text = re.sub('[^A-Za-z0-9]+', ' ', text)
+        text = re.sub('[^A-Za-z0-9\-]+', ' ', text)
         return text
 
 
     def extract_keywords(self, data, given_scores):
-        
-        spark = SparkSession.builder.getOrCreate()
         data = self.pre_precess(data)
+        # send data into RDD and count words
         data_rdd = self.sc.parallelize([data])
         counts = data_rdd.flatMap(lambda x: x.split(' ')) \
             .map(lambda x: (x, 1)) \
@@ -32,17 +29,14 @@ class Wordcount:
         output = counts.collect()
         output = dict(sorted(output, key=lambda x: x[1]))
         output_scores = []
+        # calculate the cosine similarity of the given vector and the output vector
         for skill, fraq in given_scores.items():
             if skill in output:
                 output_scores.append(fraq)
             else:
                 output_scores.append(0)
         given_scores = list(given_scores.values())
-        print(given_scores)
-        print(output_scores)
+        if all(v == 0 for v in output_scores) :
+            return 0
         result = 1 - spatial.distance.cosine(given_scores, output_scores)
-        print(result)
         return result
-        # for (word, count) in output:
-        #    print("%s: %i" % (word, count))
-        #spark.stop()
